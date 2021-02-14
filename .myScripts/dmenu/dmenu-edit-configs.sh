3afc69f4e1d2be01a8af152ee8ea6430cff723db
alkostuchkov <alkostuchkov@gmail.com>
2021-02-14 10:04:47 +0300

    Modify dmenu-edit-configs.sh oder by ABC and deleted some elements from menu

--- .myScripts/dmenu/dmenu-edit-configs.sh
+++ .myScripts/dmenu/dmenu-edit-configs.sh
@@ -7,59 +7,25 @@
 #
 # Dmenu script for editing some of my more frequently edited config files.
 
-
-# declare options=("alacritty
-# awesome
-# bash
-# broot
-# bspwm
-# doom.d/config.el
-# doom.d/init.el
-# dunst
-# dwm
-# emacs.d/init.el
-# herbstluftwm
-# i3
-# neovim
-# picom
-# polybar
-# qtile
-# quickmarks
-# qutebrowser
-# spectrwm
-# st
-# stumpwm
-# surf
-# sxhkd
-# tabbed
-# termite
-# vifm
-# vim
-# vimb
-# xmobar
-# xmonad
-# xresources
-# zsh
-# quit")
-
-declare options=("autostart_qtile
+declare options=("alacritty
+autostart_qtile
 bash
-fish
-dunst
-dmenu-edit-configs
-dmenu-system-exit
-i3
-picom
 conky
 compton
+dmenu-edit-configs
+dmenu-system-exit
+fish
+i3
+kitty
+picom
 polybar
 qtile
 qutebrowser
 rofi
 vifm
 vim
-xresources
 xterm
+xresources
 zsh
 quit")
 
@@ -67,29 +33,29 @@ choice=$(echo -e "${options[@]}" | dmenu -p 'Edit config file: ' -nb '#222B2E' -
 # choice=$(echo -e "${options[@]}" | dmenu -p 'Edit config file: ' -nb '#222B2E' -nf '#fea63c' -fn 'Ubuntu-R 12')
 
 case $choice in
-	quit)
-		echo "Program terminated." && exit 1;;
+    alacritty)
+		choice="$HOME/.config/alacritty/alacritty.yml";;
     autostart_qtile)
         choice="$HOME/.config/qtile/scripts/autostart.sh";;
 	bash)
 		choice="$HOME/.bashrc";;
-    fish)
-		choice="$HOME/.config/fish/config.fish";;
-	dunst)
-		choice="$HOME/.config/dunst/dunstrc";;
+	conky)
+		choice="$HOME/.myScripts/conky/conkyrc1";;
+	compton)
+		choice="$HOME/.config/compton.conf";;
+		# choice="$HOME/.config/compton/compton.conf";;
 	dmenu-edit-configs)
 		choice="$HOME/.myScripts/dmenu/dmenu-edit-configs.sh";;
 	dmenu-system-exit)
 		choice="$HOME/.myScripts/dmenu/dmenu-system-exit.sh";;
+    fish)
+		choice="$HOME/.config/fish/config.fish";;
 	i3)
 		choice="$HOME/.config/i3/config";;
+	kitty)
+		choice="$HOME/.config/kitty/kitty.conf";;
 	picom)
 		choice="$HOME/.config/picom/picom.conf";;
-	compton)
-		choice="$HOME/.config/compton.conf";;
-		# choice="$HOME/.config/compton/compton.conf";;
-	conky)
-		choice="$HOME/.myScripts/conky/conkyrc1";;
 	polybar)
 		choice="$HOME/.config/polybar/config";;
 	qtile)
@@ -108,11 +74,14 @@ case $choice in
 		choice="$HOME/XTerm";;
 	zsh)
 		choice="$HOME/.zshrc";;
+	quit)
+		echo "Program terminated." && exit 1;;
 	*)
 		exit 1;;
 esac
 
-terminator -e "vim $choice"
+alacritty -e vim "$choice"
+# terminator -e "vim $choice"
 
 # alacritty -e nvim "$choice"
 # emacsclient -c -a emacs "$choice"
