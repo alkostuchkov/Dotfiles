45178031d28519ba6546b3d3620df8f99c14452c
alkostuchkov <alkostuchkov@gmail.com>
2021-05-14 18:12:32 +0300

    Modify: fixed module checkupdates_plus_aur.py

--- .config/i3pystatus/modules/_usr_lib_python3_dist-packages_i3pystatus_updates/checkupdates_plus_aur.py
+++ .config/i3pystatus/modules/_usr_lib_python3_dist-packages_i3pystatus_updates/checkupdates_plus_aur.py
@@ -4,36 +4,30 @@ from i3pystatus.updates import Backend
 
 class CheckupdatesPlusAur(Backend):
     """
-    This module counts the available updates using yaourt.
-    By default it will only count aur packages. Thus it can be used with the
-    pacman backend like this:
+    This module counts the available updates (pacman and aur) using checkupdates+aur.
 
     .. code-block:: python
 
-        from i3pystatus.updates import pacman, yaourt
-        status.register("updates", backends = \
-[pacman.Pacman(), paru.Paru()])
+        from i3pystatus.updates import checkupdates_plus_aur
+        status.register(
+            "updates",
+            backends=[checkupdates_pus_aur.CheckupdatesPlusAur()]
+        )
 
-    If you want to count both pacman and aur packages with this module you can
-    set the variable count_only_aur = False like this:
-
-    .. code-block:: python
-
-        from i3pystatus.updates import paru
-        status.register("updates", backends = [paru.Paru(False)])
     """
 
-    def __init__(self, aur_only=True):
-        self.aur_only = aur_only
+    #  def __init__(self, aur_only=True):
+        #  self.aur_only = aur_only
 
     @property
     def updates(self):
         command = ["checkupdates+aur"]
         checkupdates = run_through_shell(command)
         out = checkupdates.out
-        if(self.aur_only):
-            out = [line for line in out if line.startswith("aur")]
-        return out.count("\n"), out
+        #  if(self.aur_only):
+            #  out = [line for line in out if line.startswith("aur")]
+        return len(out.split("\n")), out
+        #  return out.count("\n"), out
 
 Backend = CheckupdatesPlusAur
 
