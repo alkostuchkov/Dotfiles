7003aac186af2ec96a70805596144ee3db6e3e27
alkostuchkov <alkostuchkov@gmail.com>
2022-02-22 18:44:56 +0300

    Added-Modified: .myS/winC(D)_(un)mount.sh; changed .c/ranger/rc.conf for (un)mount Windows disks.

---
+++ .myScripts/winD_mount.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+winDisk="/dev/sda2"
+mountingResult=$(udisksctl mount -b $winDisk 2>&1)
+
+if [[ $? = 0 ]]
+then  # if mounted successfully
+    notify-send -i dialog-information -t 3000 "$(echo $mountingResult | awk '{print $1 " " $2 " " $3}')" "$(echo $mountingResult | awk '{print $4}')"
+    echo $mountingResult
+else
+    # notify-send -i dialog-error -t 3000 " " "$(echo $mountingResult | cut -d"'" -f1)"
+    errHead=$(echo ${mountingResult} | awk -F: '{print $1}')
+    errSkip=$(echo ${mountingResult} | cut -d" " -f4)
+    skipLength=`expr ${#errSkip} + ${#errHead} + 2`  # 2 = two spaces
+    errTail=$(echo ${mountingResult:$skipLength:${#mountingResult}} | cut -d"." -f1)
+    notify-send -i dialog-error -t 3000 "$errHead:" "$errTail"
+fi
---
+++ .myScripts/winD_unmount.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+winDisk="/dev/sda2"
+unmountingResult=$(udisksctl unmount -b $winDisk 2>&1)
+
+if [[ $? = 0 ]]
+then  # if unmounted successfully
+    # notify-send -i dialog-information -t 3000 "$(echo $unmountingResult | awk '{print $1 " " $2 " " $3}')" "$(echo $unmountingResult | awk '{print $4}')"
+    notify-send -i dialog-information -t 3000 "$(echo $unmountingResult | cut -d"." -f1)"
+else
+    # notify-send -i dialog-error -t 3000 " " "$(echo $unmountingResult | cut -d"'" -f1)"
+    errHead=$(echo ${unmountingResult} | awk -F: '{print $1}')
+    errSkip=$(echo ${unmountingResult} | cut -d" " -f4)
+    skipLength=`expr ${#errSkip} + ${#errHead} + 2`  # 2 = two spaces
+    errTail=$(echo ${unmountingResult:$skipLength:${#unmountingResult}} | cut -d"." -f1)
+    notify-send -i dialog-error -t 3000 "$errHead:" "$errTail"
+fi
