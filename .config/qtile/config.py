56df865cf644c6376a5f9fe7d6210e1709d5574a
alkostuchkov <alkostuchkov@gmail.com>
2022-04-15 18:50:30 +0300

    Modified: .c/qtile/config.py (added Palenight as colors); .c/ranger/rc.conf (changed Keys for mount/unmount winC and winD).

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -102,30 +102,60 @@ colors = {
     "fg_textbox": "#89ddff"  # 15
 }
 
-#  # Gruvbox Material colors
+#  # Palenight
+#  colors = {
+    #  "bg_panel": "#292d3e",  # background
+    #  "bg_current_screentab": "#585E72",  # 00
+    #  "fg_group_names": "#d0d0d0",  # 08
+    #  "bg_current_tab": "#959dcb",  # foreground
+    #  "bg_other_tabs_and_odd_widgets": "#82aaff",  # 5
+    #  "bg_even_widgets": "#9cc4ff",  # 13
+    #  "fg_windowname": "#c792ea",  # 06
+    #  "fg_cpu": "#a3f7ff",  # 15
+    #  "fg_memory": "#ffe585",  # 12
+    #  "fg_netspeed": "#ddffa7",  # 11
+    #  "fg_layout": "#d0d0d0",  # 08
+    #  "fg_keyboard": "#89ddff",  # 7
+    #  "fg_date": "#89ddff",  # 7
+    #  "bg_systray": "#292d3e",  # background
+    #  "fg_updates": "#e2e0a5",  # 00
+    #  "fg_weather": "#e1acff",  # 14
+    #  "bg_chord_dmenu": "#ffe585",  # 12
+    #  "fg_chord_dmenu": "#292d3e",  # 1
+    #  "fg_syncthing_active": "#c3e88d",  # 3
+    #  "fg_syncthing_inactive": "#ff8b92",  # 10
+    #  "fg_active_group": "#ffcb6b",  # 04
+    #  "border_focus": "#959dcb",  # foreground
+    #  "border_normal": "#1d2330",  # 00
+    #  "fg_textbox": "#a3f7ff"  # 15
+#  }
+
+#  # Gruvbox Material
 #  colors = {
    #  "bg_panel": "#282828",  # background
    #  "bg_current_screentab": "#585E72",  # 00
-   #  "fg_group_names": "#89b482",  # 00
-   #  "bg_current_tab": "#665c54",  # 00
+   #  #  "fg_group_names": "#89b482",  # 00
+   #  #  "bg_current_tab": "#665c54",  # 00
+   #  "fg_group_names": "#ffffff",  # 08+
+   #  "bg_current_tab": "#dfbf8e",  # foreground+
    #  "bg_other_tabs_and_odd_widgets": "#7daea3",  # 5
    #  "bg_even_widgets": "#7daea3",  # 13
-   #  "fg_windowname": "#e3a84e",  # 00
+   #  "fg_windowname": "#d3869b",  # 06+
    #  "fg_cpu": "#89b482",  # 15
    #  "fg_memory": "#e3a84e",  # 12
    #  "fg_netspeed": "#a9b665",  # 11
-   #  "fg_layout": "#dfbf8e",  # foreground
+   #  "fg_layout": "#ffffff",  # 08+
    #  "fg_keyboard": "#89b482",  # 7
    #  "fg_date": "#89b482",  # 7
    #  "bg_systray": "#282828",  # background
    #  "fg_updates": "#e2e0a5",  # 00
-   #  "fg_weather": "#d3869b",  # 00
+   #  "fg_weather": "#d3869b",  # 14+
    #  "bg_chord_dmenu": "#e3a84e",  # 12
    #  "fg_chord_dmenu": "#665c54",  # 1
    #  "fg_syncthing_active": "#a9b665",  # 3
    #  "fg_syncthing_inactive": "#ea6962",  # 10
-   #  "fg_active_group": "#e3a84e",  # 12
-   #  "border_focus": "#d79921",  # 00
+   #  "fg_active_group": "#e78a4e",  # 04+
+   #  "border_focus": "#dfbf8e",  # foreground+
    #  "border_normal": "#1d2330",  # 00
    #  "fg_textbox": "#89b482"  # 15
 #  }
