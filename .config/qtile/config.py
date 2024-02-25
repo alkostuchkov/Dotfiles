1481315d041506bbbdd8c168b9858d27c2f29712
alkostuchkov <alkostuchkov@gmail.com>
2024-02-25 14:19:22 +0300

    Modified: .c/qtile/config.py (Move/Resize floating window keybindings like in BSPWM an AwesomeWM).

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -20,12 +20,12 @@ from libqtile.backend.base import Internal
 
 ##### IMPORTs from my modules #####
 from modules import (
-    colors,
-    memory,
-    all_windows_count,
-    syncthing,
-    show_updates,
-)
+        colors,
+        memory,
+        all_windows_count,
+        syncthing,
+        show_updates,
+        )
 
 
 def get_all_netifaces(path_to_state="/sys/class/net/"):
@@ -86,33 +86,33 @@ upped_netiface = get_upped_netiface(netifaces)
 
 ##### COLORS #####
 # All color schemes are in dir colors. This theme is for example.
-# # Materia Manjaro
-# colors = {
-#     "bg_panel": "#263238",  # background
-#     "bg_current_screentab": "#585E72",  # 00
-#     "fg_group_names": "#dbdcd5",  # 00 (08)
-#     "bg_current_tab": "#009185",  # 00 (foreground)
-#     "bg_other_tabs_and_odd_widgets": "#6182b8",  # 5
-#     "bg_even_widgets": "#82aaff",  # 13
-#     "fg_windowname": "#24d2af",  # 00 (06)
-#     "fg_cpu": "#89ddff",  # 15
-#     "fg_memory": "#ffcb6b",  # 12
-#     "fg_netspeed": "#c3e88d",  # 11
-#     "fg_layout": "#eeffff",  # foreground (08)
-#     "fg_keyboard": "#39adb5",  # 7
-#     "fg_date": "#39adb5",  # 7
-#     "bg_systray": "#263238",  # background
-#     "fg_updates": "#e2e0a5",  # 00
-#     "fg_weather": "#eb7bef",  # 00 (14)
-#     "bg_chord_dmenu": "#ffcb6b",  # 12
-#     "fg_chord_dmenu": "#000000",  # 1
-#     "fg_syncthing_active": "#91b859",  # 3
-#     "fg_syncthing_inactive": "#ff5370",  # 10
-#     "fg_active_group": "#e4c962",  # 00 (04)
-#     "border_focus": "#2eb398",  # 00 (foreground)
-#     "border_normal": "#1d2330",  # 00
-#     "fg_textbox": "#89ddff"  # 15
-# }
+#  # Materia Manjaro
+#  colors = {
+    #  "bg_panel": "#263238",  # background
+    #  "bg_current_screentab": "#585E72",  # 00
+    #  "fg_group_names": "#dbdcd5",  # 00 (08)
+    #  "bg_current_tab": "#009185",  # 00 (foreground)
+    #  "bg_other_tabs_and_odd_widgets": "#6182b8",  # 5
+    #  "bg_even_widgets": "#82aaff",  # 13
+    #  "fg_windowname": "#24d2af",  # 00 (06)
+    #  "fg_cpu": "#89ddff",  # 15
+    #  "fg_memory": "#ffcb6b",  # 12
+    #  "fg_netspeed": "#c3e88d",  # 11
+    #  "fg_layout": "#eeffff",  # foreground (08)
+    #  "fg_keyboard": "#39adb5",  # 7
+    #  "fg_date": "#39adb5",  # 7
+    #  "bg_systray": "#263238",  # background
+    #  "fg_updates": "#e2e0a5",  # 00
+    #  "fg_weather": "#eb7bef",  # 00 (14)
+    #  "bg_chord_dmenu": "#ffcb6b",  # 12
+    #  "fg_chord_dmenu": "#000000",  # 1
+    #  "fg_syncthing_active": "#91b859",  # 3
+    #  "fg_syncthing_inactive": "#ff5370",  # 10
+    #  "fg_active_group": "#e4c962",  # 00 (04)
+    #  "border_focus": "#2eb398",  # 00 (foreground)
+    #  "border_normal": "#1d2330",  # 00
+    #  "fg_textbox": "#89ddff"  # 15
+#  }
 
 ##### KEYBINDINGS #####
 keys = [
@@ -321,8 +321,8 @@ for key, x, y in [
         ("Right", 10, 0),
         ("Up", 0, -10),
         ("Down", 0, 10)]:
-    keys.append(Key([mod, "shift"], key, lazy.window.move_floating(x, y)))
-    keys.append(Key([mod, "control"], key, lazy.window.resize_floating(x, y)))
+    keys.append(Key([mod, "control"], key, lazy.window.move_floating(x, y)))
+    keys.append(Key([mod, "shift"], key, lazy.window.resize_floating(x, y)))
 
 ##### GROUPS #####
 #       v                   
