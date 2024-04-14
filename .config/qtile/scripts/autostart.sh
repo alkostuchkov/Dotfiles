2291071aad5eeac67441c59e516864d86eac4d1d
alkostuchkov <alkostuchkov@gmail.com>
2024-04-14 23:54:19 +0300

    Modified: .c/qtile/scripts/autostart.sh (xfce-polkit instead of polkit-gnome).

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash 
 
+# picom --experimental-backends &
+# compton --config $HOME/.config/compton/compton.conf &
+# compton --config $HOME/.config/compton.conf &
+# diodon &
+# /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/kbdd &
 setxkbmap -layout us,ru -option grp:caps_toggle &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
-# /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & # for Debian
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &  # for Arch
-/usr/libexec/polkit-gnome-authentication-agent-1 & # for Void
+# /usr/libexec/polkit-gnome-authentication-agent-1 &  # for Void
+/usr/libexec/xfce-polkit &  # for Void
 xrdb $HOME/.Xresources &
 nitrogen --restore &
 volumeicon &
