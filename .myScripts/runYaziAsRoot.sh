f4158866dba469d53cddeafc64f42d2a2bea3117
alkostuchkov <alkostuchkov@gmail.com>
2024-06-23 20:56:16 +0300

    Added: .myS/runYaziAsRoot.sh

---
+++ .myScripts/runYaziAsRoot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Run yazi as root for Qtile
+# pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY dbus-run-session $TERMINAL -e yazi
+pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY dbus-run-session alacritty -e yazi
