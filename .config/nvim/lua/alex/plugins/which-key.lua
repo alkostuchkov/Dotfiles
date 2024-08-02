5dad96e2a4a48a28c0d597b416bee6a00ee06bbc
alkostuchkov <alkostuchkov@gmail.com>
2024-08-02 23:29:21 +0300

    Added: .c/nvim/lua/alex/plugins/which-key.lua (no icons).

--- .config/nvim/lua/alex/plugins/which-key.lua
+++ .config/nvim/lua/alex/plugins/which-key.lua
@@ -1,14 +1,17 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  dependences = {
-    { 'echasnovski/mini.icons', version = false },
-  },
+  -- dependences = {
+  --   { 'echasnovski/mini.icons', version = false },
+  -- },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
   opts = {
+    icons = {
+      mappings = false,
+    },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
