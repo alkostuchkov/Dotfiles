9b33272e0dfe95252ed0840c82a7ebfe5f23bdb7
alkostuchkov <alkostuchkov@gmail.com>
2024-08-10 21:58:11 +0300

    Modified: comment.lua

--- .config/nvim/lua/alex/plugins/comment.lua
+++ .config/nvim/lua/alex/plugins/comment.lua
@@ -7,6 +7,7 @@ return {
   config = function()
     -- import comment plugin safely
     local comment = require("Comment")
+    -- local ft = require("Comment.ft")
 
     local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
@@ -15,5 +16,7 @@ return {
       -- for commenting tsx, jsx, svelte, html files
       pre_hook = ts_context_commentstring.create_pre_hook(),
     })
+
+    -- ft.rc = '" %s'
   end,
 }
