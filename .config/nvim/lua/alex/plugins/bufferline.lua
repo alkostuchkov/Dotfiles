bf53cb89e525c40d068ef45d721788fd47cf4917
alkostuchkov <alkostuchkov@gmail.com>
2024-08-15 23:21:49 +0300

    Modified: .c/nvim/../plugins/bufferline.lua
    Changed: bufferline -> buffer_close_icon = "<U+F00D>"

--- .config/nvim/lua/alex/plugins/bufferline.lua
+++ .config/nvim/lua/alex/plugins/bufferline.lua
@@ -7,6 +7,12 @@ return {
       mode = "tabs",
       -- mode = "buffers",
       separator_style = "slop",
+      -- buffer_close_icon = '󰅖',
+      buffer_close_icon = "",
+      modified_icon = "●",
+      close_icon = "",
+      left_trunc_marker = "",
+      right_trunc_marker = "",
     },
   },
 }
