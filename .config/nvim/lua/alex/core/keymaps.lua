ba8cbac3af8f76d38a546ae25c374515e93507cb
alkostuchkov <alkostuchkov@gmail.com>
2024-09-14 21:50:13 +0300

    modified: .c/nvim/../keymaps.lua, .c/nvim/../nvim-tree.lua

--- .config/nvim/lua/alex/core/keymaps.lua
+++ .config/nvim/lua/alex/core/keymaps.lua
@@ -96,3 +96,23 @@ keymap.set("i", "<C-Down>", "<Esc>:m .+1<CR>==gi")
 
 -- usefull replacement example
 keymap.set("v", "<C-r>", "\"hy:%s/<C-r>h//gc<left><left><left>", { desc = "Replace selected" })
+
+-- folding
+vim.opt.foldcolumn = "0"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+
+vim.opt.foldnestmax = 3
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+local function close_all_folds()
+  vim.api.nvim_exec2("%foldc!", { output = false })
+end
+local function open_all_folds()
+  vim.api.nvim_exec2("%foldo!", { output = false })
+end
+
+vim.keymap.set("n", "<leader>zs", close_all_folds, { desc = "[s]hut all folds" })
+vim.keymap.set("n", "<leader>zo", open_all_folds, { desc = "[o]pen all folds" })
