d2fee1d4cae2a0a99f99c97d796a6c40729092a8
alkostuchkov <alkostuchkov@gmail.com>
2024-12-16 22:57:14 +0300

    modified: .c/awesome/rc.lua (Iosevka for notifications).

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -17,7 +17,8 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 naughty.config.defaults["icon_size"] = 100
 naughty.config.defaults["timeout"] = 5
-naughty.config.defaults["font"] = "Sarasa Mono SC Nerd 13"
+-- naughty.config.defaults["font"] = "Sarasa Mono SC Nerd 13"
+naughty.config.defaults["font"] = "Iosevka 13"
 naughty.config.defaults["border_width"] = 1
 
 local menubar = require("menubar")
