46ff1a0b584534e02852eafe8cfcd9a8f5e373f2
alkostuchkov <alkostuchkov@gmail.com>
2025-03-11 21:58:04 +0300

    modified: .myS/volume_up(down).sh fixed for BY

--- .myScripts/volume_down.sh
+++ .myScripts/volume_down.sh
@@ -5,7 +5,8 @@
 
 pactl set-sink-volume 0 -5%
 
-current_volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+# current_volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+current_volume=$(pactl list sinks | grep '^[[:space:]]\(Volume:\)\|\(Громкость:\)\|\(Гучнасць:\)' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
 notify-send -t 1000 -i dialog-information "Volume: $current_volume%"
 
