e0040fc233695992e18da99fa38b7eb1113da227
alkostuchkov <alkostuchkov@gmail.com>
2025-12-14 22:00:18 +0300

    new file:   .myScripts/dmscripts/dm-wanna-read.sh
    Script to quick acces to books I wanna read.

---
+++ .myScripts/dmscripts/dm-wanna-read.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+# Dmenu script for choice a book I wanna read right now.
+
+options=(
+"$HOME/Documents/Books/"
+"$HOME/Documents/Books/Родная_мова/Кнігі/"
+"$HOME/Documents/Books/Родная_мова/Падручнікі/"
+"$HOME/Documents/Books/Родная_мова/Слоўнікі/"
+"$HOME/Documents/Books/Programming/Algorithms/"
+"$HOME/Documents/Books/Programming/Android/"
+"$HOME/Documents/Books/Programming/ASM/"
+"$HOME/Documents/Books/Programming/C/"
+"$HOME/Documents/Books/Programming/C++/"
+"$HOME/Documents/Books/Programming/DB/"
+"$HOME/Documents/Books/Programming/Electronic/"
+"$HOME/Documents/Books/Programming/Encryption/"
+"$HOME/Documents/Books/Programming/Git/"
+"$HOME/Documents/Books/Programming/Go/"
+"$HOME/Documents/Books/Programming/Haskell/"
+"$HOME/Documents/Books/Programming/IT_ebooks/"
+"$HOME/Documents/Books/Programming/Java/"
+"$HOME/Documents/Books/Programming/LISP/EmacsLISP/"
+"$HOME/Documents/Books/Programming/LISP/CommonLISP/"
+"$HOME/Documents/Books/Programming/Lua/"
+"$HOME/Documents/Books/Programming/Markdown/"
+"$HOME/Documents/Books/Programming/Math/"
+"$HOME/Documents/Books/Programming/Memorization/"
+"$HOME/Documents/Books/Programming/Networks/"
+"$HOME/Documents/Books/Programming/OAuth/"
+"$HOME/Documents/Books/Programming/Other/"
+"$HOME/Documents/Books/Programming/Pascal_Delphi_Lazarus/"
+"$HOME/Documents/Books/Programming/Python/"
+"$HOME/Documents/Books/Programming/Rust/"
+"$HOME/Documents/Books/Programming/Shell/"
+"$HOME/Documents/Books/Programming/StackOverflow_Learning_Books/"
+"$HOME/Documents/Books/Programming/Tmux_terminal/"
+"$HOME/Documents/Books/Programming/Vim/"
+"$HOME/Documents/Books/Programming/Web/"
+)
+
+# Colors:
+# # Materia Manjaro
+# nf='#09dbc9'
+# nb='#222b2e'
+# sf='#dbdcd5'
+# sb='#009185'
+
+# # Everforest
+# nf='#d3c6aa'
+# nb='#2d353b'
+# sf='#a7c080'
+# sb='#475258'
+# fn='Iosevka-17:normal'
+# fn='Ubuntu-17:normal'
+fn='JetbrainsMonoNerdFont-16:normal'
+
+# MyBlue
+nf='#CFD6DF'
+nb='#32343D'
+sf='#F9F9F9'
+sb='#3D5E87'
+
+# Gruvbox
+# nf='#fea63c'
+# nb='#282828'
+# # sf='#dbdcd5'
+# sb='#d79921'
+# fn='Sarasa Mono SC Nerd-17:normal'
+
+# dmenu colors
+# DMENU="rofi -dmenu -theme-str 'window {width: 80%;}' -p"
+DMENU="dmenu -i -l 10 -nf ${nf} -nb ${nb} -sf ${sf} -sb ${sb} -fn ${fn} -p"
+
+get_dirpath() {
+    # Get dirpath.
+    dirpath=$(printf '%s\n' "${options[@]}" | ${DMENU} 'Choose path:')
+    # dirpath=$(echo "$dirpath" | awk -F: '{print $NF}')
+    # dirpath=$(printf '%s\n' "${options[@]}" | awk -F: '{print $NF}' | ${DMENU} 'Choose path:')
+    check_input "$dirpath"
+}
+
+get_bookname() {
+    # Get bookname of chosen book.
+    bookname=$(ls "$dirpath" | ${DMENU} 'Choose book:')
+    check_input "$bookname"
+
+    while [[ -d "$dirpath$bookname" ]]; do
+        dirpath=$dirpath$bookname/
+        get_bookname "$dirpath"
+    done
+}
+
+get_extention() {
+    # Get extention of chosen book.
+    ext=${bookname##*.}
+}
+
+open_book() {
+    # Open chosen book in the sertain app depending of the extention.
+    case $ext in
+        pdf|djvu) zathura "$dirpath$bookname" || qpdfview "$dirpath$bookname";;
+        fb2|epub) foliate "$dirpath$bookname" || FBReader "$dirpath$bookname";;
+        *) notify-send -t 5000 -i dialog-information "Unknown book extention: $ext";;
+    esac
+}
+
+check_input() {
+    # Exit program if chosen "Quit".
+    if [[ "$1" == "" ]]; then
+        notify-send -t 3000 -i dialog-information "Program terminated" && exit 1
+    fi
+}
+
+get_dirpath
+get_bookname
+get_extention
+open_book
+
