af943116a752499824737657381df582d0511d78
alkostuchkov <alkostuchkov@gmail.com>
2026-02-27 20:20:37 +0300

    modified:   .config/nvim/lua/alex/plugins/which-key.lua
    Added some new options like border, preset...

--- .config/nvim/lua/alex/plugins/which-key.lua
+++ .config/nvim/lua/alex/plugins/which-key.lua
@@ -1,9 +1,9 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  -- dependences = {
-  --   { 'echasnovski/mini.icons', version = false },
-  -- },
+  dependences = {
+    { 'echasnovski/mini.icons', version = false },
+  },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
@@ -15,5 +15,13 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+  preset = "classic", -- "classic", "modern", "helix"
+  layout = {
+      width = { min = 30 }, -- min and max width of the columns
+      spacing = 3, -- spacing between columns
+    },
+  win = {
+      border = "rounded", -- Options: "rounded", "single", "double", "solid", "none"
+    },
   },
 }
