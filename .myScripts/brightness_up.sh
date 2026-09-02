f78373dc72687c0373afa95964b801ef4cf65796
alkostuchkov <alkostuchkov@gmail.com>
2026-09-02 17:42:33 +0300

    modified:   .config/niri/config.kdl             .myScripts/brightness_down.sh             .myScripts/brightness_up.sh
                .local/state/noctalia/settings.toml
    
    fixed:
      brightness notifications;
      hotkeys to call noctalia's panels and others...

--- .myScripts/brightness_up.sh
+++ .myScripts/brightness_up.sh
@@ -5,17 +5,22 @@ current_brightness=$(cat /sys/class/backlight/radeon_bl0/brightness)
 
 # current_brightness=$(expr $current_brightness + 12)
 current_brightness=$((current_brightness + 5))
+current_percents=$(((current_brightness * 100) / 255))
 
 if [ $current_brightness -le $max_brightness ]
     then
         echo $current_brightness > /sys/class/backlight/radeon_bl0/brightness
         echo ${current_brightness} > $HOME/.myScripts/current_brightness.txt
-        notify-send -t 1000 -i dialog-information "Brightness $current_brightness"
+        # notify-send -t 1000 -i dialog-information -a "" "Brightness: $current_brightness"
+
+        noctalia msg brightness-osd $current_percents
     else
         current_brightness=$max_brightness
         echo $current_brightness > /sys/class/backlight/radeon_bl0/brightness
         echo ${current_brightness} > $HOME/.myScripts/current_brightness.txt
-        notify-send -t 1000 -i dialog-information "Brightness $current_brightness"
+        # notify-send -t 1000 -i dialog-information -a "" "Brightness: $current_brightness"
+
+        noctalia msg brightness-osd $current_percents
 fi
 
 # brightnessctl set +5
